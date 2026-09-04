842545016e77fe1b6e036ef75e2a34bfbb296dff
Amr MOUSA <amr.mo.abdelgawad@gmail.com>
2026-09-04 03:48:22 +0300

    chore: update Homebrew checksums for v0.1.4 (#9)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- homebrew/devctl.rb
+++ homebrew/devctl.rb
@@ -1,26 +1,29 @@
 class Devctl < Formula
   desc "Local development orchestrator"
   homepage "https://github.com/amr-m-abdelgawad/devctl"
-  version "0.1.2"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/amr-m-abdelgawad/devctl/releases/download/v#{version}/devctl-darwin-arm64"
+      sha256 "6745f70ed73ccfdaff3ce14eef402f6751bd60e30d49183059825ac7ca93795f"
     else
       url "https://github.com/amr-m-abdelgawad/devctl/releases/download/v#{version}/devctl-darwin-x64"
+      sha256 "8c97d7563fb9279b5771a346a8a21fc2d98c1e2b2fe16fc38af80f5b09e545a8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/amr-m-abdelgawad/devctl/releases/download/v#{version}/devctl-linux-arm64"
+      sha256 "d73a83fd5b333f7283de9a6bb324312699436e4f9e96b91ee6b665e12d407e1b"
     else
       url "https://github.com/amr-m-abdelgawad/devctl/releases/download/v#{version}/devctl-linux-x64"
+      sha256 "71882439bf6284db5326e0593f36d18207828f08da8fffdea491273df5631421"
     end
   end
 
-  sha256 :no_check
 
   def install
     bin.install Dir["devctl*"].first => "devctl"
